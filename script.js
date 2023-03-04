daa7750abf43e43f1b430db72fecfdbd9bf42813
Jaunty5899 <jaunty5899@gmail.com>
2023-03-04 15:58:03 +0530

    confetti added

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 let items = [];
-const button = document.querySelector("button");
+const form = document.querySelector("form");
 const input = document.querySelector("#to_input");
+
+const add_item = (item) => {
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.innerText = item;
+  ul.appendChild(li);
+  // party.confetti(this);
+  document.querySelector("#confetti").click();
+};
 
 const submit_item = () => {
   const new_item = input.value.trim();
   if (new_item !== "") {
     input.value = "";
     items.push(new_item);
-    console.log({ new_item });
-    console.log({ items });
+    // console.log({ new_item });
+    // console.log({ items });
+    add_item(new_item);
   }
 };
 
-button.addEventListener(
-  "click",
-  submit_item
+form.addEventListener(
+  "submit",
+  (event) => {
+    event.preventDefault();
+    submit_item();
+  }
+
   // console.log({ new_item });
   // console.log({ items });
 );
 
-input.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") submit_item();
+// input.addEventListener("keypress", (event) => {
+//   if (event.key === "Enter") submit_item();
+// });
+document.querySelector("#confetti").addEventListener("click", function () {
+  party.confetti(this);
 });
