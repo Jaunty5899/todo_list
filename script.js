f07dbe4abccd8092b14fee7d63c09ff8606539c5
Jaunty5899 <jaunty5899@gmail.com>
2023-03-04 13:05:32 +0530

    added enter feature

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let items = [];
 const button = document.querySelector("button");
 const input = document.querySelector("#to_input");
-button.addEventListener("click", () => {
+
+const submit_item = () => {
   const new_item = input.value;
   input.value = "";
   items.push(new_item);
+  console.log({ new_item });
+  console.log({ items });
+};
 
-  //   console.log({ new_item });
-  //   console.log({ items });
+button.addEventListener(
+  "click",
+  submit_item
+  // console.log({ new_item });
+  // console.log({ items });
+);
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") submit_item();
 });
